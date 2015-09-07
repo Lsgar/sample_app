483edc8268e4142707431893aa4ce13f16efc5ff
garp <l_s_gar@yahoo.co.jp>
2015-09-07 08:31:28 +0000

    7.6 演習　完了！

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,37 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission (all blank)" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email can\'t be blank') }
+        it { should have_content('Password can\'t be blank') }
+      end
+
+      describe "after submission (email invalid)" do
+        before do
+          fill_in "Email",        with: "user_at_example.com"
+          click_button submit
+        end
+
+        it { should have_content('Email is invalid') }
+      end
+
+      describe "after submission (Password too short, Confirmation blank)" do
+        before do
+          fill_in "Password",     with: "short"
+          fill_in "Confirmation", with: " "
+          click_button submit
+        end
+
+        it { should have_content('Password is too short (minimum is 6 characters)') }
+        it { should have_content('Password confirmation can\'t be blank') }
+        it { should have_content('Password confirmation doesn\'t match Password') }
+      end
     end
 
     describe "with valid information" do
@@ -41,6 +72,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
