4250149f23c475527e54d3b031f8245f4de2868a
garp <l_s_gar@yahoo.co.jp>
2015-09-14 09:15:24 +0000

    mod errors_controller.rb

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ActionController::Base
   layout 'application'
 
+  rescue_from StandardError, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from StandardError, with: :render_500
 
   def render_404(exception = nil)
     logger.info "Rendering 404 with exception: #{exception.message}" if exception
